f97242b9edd936714a0c85d6b2e61223dc858742
Omar Rodriguez E <a01154888@gmail.com>
2016-03-18 21:03:55 +0000

    columnas register

--- views/register_form.php
+++ views/register_form.php
@@ -1,31 +1,46 @@
-<form action="register.php" method="post">
-    <fieldset>
-        <div class="form-group">
-            <input autocomplete="off" autofocus class="form-control" name="name" placeholder="First Name" type="text"/>
-        </div>
-        <div class="form-group">
-            <input autocomplete="off" autofocus class="form-control" name="last_name" placeholder="Last Name" type="text"/>
-        </div>
-        <div class="form-group">
-            <input autocomplete="off" autofocus class="form-control" name="username" placeholder="Username" type="text"/>
-        </div>
-        <div class="form-group">
-            <input class="form-control" name="password" placeholder="Password" type="password"/>
-        </div>
-        <div class="form-group">
-            <input class="form-control" name="confirmation" placeholder="Confirmation" type="password"/>
-        </div>
-        <select class="form-control" name="symbol">
-                <option value="Symbol">Select Avatar</option>
-                <option value="Symbol">Cheetah</option>
-                <option value="Symbol">Elephant</option>
-                <option value="Symbol">Giraffe</option>
-                <option value="Symbol">Hare</option>
-                <option value="Symbol">Red Panda</option>
-                <option value="Symbol">Tiger</option>
-                <option value="Symbol">Owl</option>
-                <option value="Symbol">Fox</option>
-        </select>
+<div class="row">
+    <div class="col-md-4"></div>
+    <div class="col-md-4">
+        <form action="register.php" method="post">
+            <fieldset>
+                <div class="form-group">
+                    <input autocomplete="off" autofocus class="form-control" name="name" placeholder="First Name" type="text"/>
+                </div>
+                <div class="form-group">
+                    <input autocomplete="off" autofocus class="form-control" name="last_name" placeholder="Last Name" type="text"/>
+                </div>
+                <div class="form-group">
+                    <input autocomplete="off" autofocus class="form-control" name="username" placeholder="Username" type="text"/>
+                </div>
+                <div class="form-group">
+                    <input class="form-control" name="password" placeholder="Password" type="password"/>
+                </div>
+                <div class="form-group">
+                    <input class="form-control" name="confirmation" placeholder="Confirmation" type="password"/>
+                </div>
+                <div class="form-group">
+                    <select class="form-control" name="symbol">
+                            <option value="Symbol">Select Avatar</option>
+                            <option value="cheetah.jpg">Cheetah</option>
+                            <option value="elephant.jpg">Elephant</option>
+                            <option value="giraffe.jpg">Giraffe</option>
+                            <option value="hare.jpg">Hare</option>
+                            <option value="panda.jpg">Red Panda</option>
+                            <option value="tiger.jpg">Tiger</option>
+                            <option value="owl.jpg">Owl</option>
+                            <option value="fox.jpg">Fox</option>
+                    </select>
+                </div>
+                <div class="form-group">
+                    <button class="btn btn-default" type="submit">
+                        <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
+                        Register
+                    </button>
+                </div>
+            </fieldset>
+        </form>
+    </div>
+    <div class="col-md-4">
         <table>
             <thead>
                 <tr>
@@ -46,19 +61,14 @@
             	<td><div><img width ="110px" alt="tiger" src="/img/tiger.jpg"/></a></div></td>
             </tr>
             <tr>
-            	<td><div><img width ="110px" alt="owl" src="/img/owl.jpg"/></a></div></td>
+            	<td><div></div><img width ="110px" alt="owl" src="/img/owl.jpg"/></a></div></td>
             	<td><div><img width ="110px" alt="fox" src="/img/fox.jpg"/></a></div></td>
             </tr>
             </tbody>
         </table>
-        <div class="form-group">
-            <button class="btn btn-default" type="submit">
-                <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
-                Register
-            </button>
-        </div>
-    </fieldset>
-</form>
+
+    </div>
+</div>
 <div>
     or <a href="login.php">log in</a>
 </div>
