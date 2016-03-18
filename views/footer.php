fee60be2c6bff49ad1318a83ab0b6c83243cbd83
Omar Rodriguez E <a01154888@gmail.com>
2016-03-18 19:05:06 +0000

    Image of the footer

--- views/footer.php
+++ views/footer.php
@@ -3,5 +3,8 @@
                 <div id="bottom">
                         Brought to you by the number <a href="http://cdn.cs50.net/2015/fall/psets/7/pset7/pset7.html">7</a>.
                 </div>
+                <div align="left">
+                        <img src="https://ads5.files.wordpress.com/2011/12/b41.jpg" alt="Fotter" style="width:1143px; height:228px;">
+                </div>
         </body>
 </html>
