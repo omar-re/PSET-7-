12cd667533bfca335908ce2003033b13c3a8c1df
Omar Rodriguez E <a01154888@gmail.com>
2016-03-18 20:27:13 +0000

    images of avatars

--- views/register_form.php
+++ views/register_form.php
@@ -15,6 +15,31 @@
         <div class="form-group">
             <input class="form-control" name="confirmation" placeholder="Confirmation" type="password"/>
         </div>
+        <table>
+            <thead>
+                <tr>
+                	<th>Choose your avatar</th>
+                </tr>
+            </thead>
+            <tbody align = "center">
+            <tr>
+            	<td><div><img width ="110px" alt="cheeta" src="/img/cheetha.jpg"/></a></div></td>
+            	<td><div><img width ="110px" alt="elephant" src="/img/elephant.jpg"/></a></div></td>
+            </tr>
+            <tr>
+            	<td><div><img width ="110px" alt="giraffe" src="/img/giraffe.jpg"/></a></div></td>
+            	<td><div><img width ="110px" alt="hare" src="/img/hare.jpg"/></a></div></td>
+            </tr>
+            <tr>
+            	<td><div><img width ="110px" alt="panda" src="/img/panda.jpg"/></a></div></td>
+            	<td><div><img width ="110px" alt="tiger" src="/img/tiger.jpg"/></a></div></td>
+            </tr>
+            <tr>
+            	<td></td><div><img width ="110px" alt="owl" src="/img/owl.jpg"/></a></div></td>
+            	<td><div><img width ="110px" alt="fox" src="/img/fox.jpg"/></a></div></td>
+            </tr>
+            </tbody>
+        </table>
         <div class="form-group">
             <button class="btn btn-default" type="submit">
                 <span aria-hidden="true" class="glyphicon glyphicon-log-in"></span>
